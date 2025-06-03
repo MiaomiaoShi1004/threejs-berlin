be6baa82a4adb67550269950d479140c510e9b12
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2025-06-03 23:22:14 +0200

    center tile and put in eye level

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-// src/main.js
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js'
@@ -6,23 +5,37 @@ import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js'
 
 export function initScene() {
   console.log("🚀 initScene() has been called")
+
   const canvas = document.getElementById('webglCanvas')
   const scene = new THREE.Scene()
   const sizes = { width: window.innerWidth, height: window.innerHeight }
+
   const camera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height, 0.1, 2000)
-  camera.position.set(100, 200, 400)
+  camera.position.set(0, 300, 300)
   camera.lookAt(0, 0, 0)
   scene.add(camera)
+
   const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true })
   renderer.setSize(sizes.width, sizes.height)
   renderer.setPixelRatio(window.devicePixelRatio)
+
   const dirLight = new THREE.DirectionalLight(0xffffff, 1)
   dirLight.position.set(300, 400, 200)
   scene.add(dirLight)
+
   const ambLight = new THREE.AmbientLight(0x888888)
   scene.add(ambLight)
+
   const controls = new OrbitControls(camera, renderer.domElement)
   controls.enableDamping = true
+  controls.target.set(0, 0, 0)
+  controls.update()
+
+  controls.minPolarAngle = THREE.MathUtils.degToRad(90);
+  controls.maxPolarAngle = THREE.MathUtils.degToRad(160);
+  controls.minAzimuthAngle = 0;
+  controls.maxAzimuthAngle = 0;
+
   const mtlLoader = new MTLLoader()
   mtlLoader.setPath('/assets/Tile-112-73-1-1/')
   mtlLoader.load(
@@ -30,6 +43,7 @@ export function initScene() {
     (materials) => {
       console.log('🟢 MTL loaded successfully')
       materials.preload()
+
       const objLoader = new OBJLoader()
       objLoader.setMaterials(materials)
       objLoader.setPath('/assets/Tile-112-73-1-1/')
@@ -37,9 +51,11 @@ export function initScene() {
         'Tile-112-73-1-1.obj',
         (objGroup) => {
           console.log('🟢 OBJ loaded successfully')
+          // Center the tile:
           const bbox = new THREE.Box3().setFromObject(objGroup)
           const center = bbox.getCenter(new THREE.Vector3())
           objGroup.position.sub(center)
+
           scene.add(objGroup)
           console.log('✅ Berlin tile added to scene')
         },
@@ -58,6 +74,7 @@ export function initScene() {
       console.error('Error loading MTL:', error)
     }
   )
+
   window.addEventListener('resize', () => {
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -65,6 +82,7 @@ export function initScene() {
     camera.updateProjectionMatrix()
     renderer.setSize(sizes.width, sizes.height)
   })
+
   function animate() {
     requestAnimationFrame(animate)
     controls.update()
